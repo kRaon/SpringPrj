344357509bf378c1276e14bfd2dca803988c3fa3
장지용 <tatagi@naver.com>
2018-02-21 11:14:11 +0900

    cc

--- app/src/main/java/com/my/biz/service/Asset_activityService.java
+++ app/src/main/java/com/my/biz/service/Asset_activityService.java
@@ -2,6 +2,7 @@ package com.my.biz.service;
 
 import java.util.List;
 
+import com.my.biz.vo.ActivityCategoriesVO;
 import com.my.biz.vo.Asset_activityVO;
 
 public interface Asset_activityService {
@@ -14,6 +15,7 @@ public interface Asset_activityService {
 	public List<Asset_activityVO> selectAllActivities();
 
 	public int selectActivityNum(String category_name);
-	
+
+	public List<ActivityCategoriesVO> selectAllActivityCatrgories();
 }
 
---
+++ app/src/main/java/com/my/biz/vo/ActivityCategoriesVO.java
@@ -0,0 +1,104 @@
+package com.my.biz.vo;
+
+import java.sql.Date;
+
+public class ActivityCategoriesVO {
+
+	int indexnumber;
+	String id;
+	int category_name;
+	String contents;
+	int amount;
+	String fixed;
+	Date a_date;
+	String a_type;
+
+	public ActivityCategoriesVO() {
+		super();
+	}
+
+	public ActivityCategoriesVO(int indexnumber, String id, int category_name, String contents, int amount,
+			String fixed, Date a_date, String a_type) {
+		super();
+		this.indexnumber = indexnumber;
+		this.id = id;
+		this.category_name = category_name;
+		this.contents = contents;
+		this.amount = amount;
+		this.fixed = fixed;
+		this.a_date = a_date;
+		this.a_type = a_type;
+	}
+
+	public int getIndexnumber() {
+		return indexnumber;
+	}
+
+	public void setIndexnumber(int indexnumber) {
+		this.indexnumber = indexnumber;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getCategory_name() {
+		return category_name;
+	}
+
+	public void setCategory_name(int category_name) {
+		this.category_name = category_name;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getFixed() {
+		return fixed;
+	}
+
+	public void setFixed(String fixed) {
+		this.fixed = fixed;
+	}
+
+	public Date getA_date() {
+		return a_date;
+	}
+
+	public void setA_date(Date a_date) {
+		this.a_date = a_date;
+	}
+
+	public String getA_type() {
+		return a_type;
+	}
+
+	public void setA_type(String a_type) {
+		this.a_type = a_type;
+	}
+
+	@Override
+	public String toString() {
+		return "ActivityCategoriesVO [indexnumber=" + indexnumber + ", id=" + id + ", category_name=" + category_name
+				+ ", contents=" + contents + ", amount=" + amount + ", fixed=" + fixed + ", a_date=" + a_date
+				+ ", a_type=" + a_type + "]";
+	}
+
+}
