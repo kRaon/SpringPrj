9e7d7d0eff2e9d0047894c87b1b06ce89bc5585a
지환 <qwq713@naver.com>
2018-02-23 10:32:30 +0900

    재무설계완료

---
+++ app/src/main/java/com/my/web/controller/test.java
@@ -0,0 +1,12 @@
+package com.my.web.controller;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1000000;
+		String str = String.format("%,d", i);
+		System.out.println(str);
+	}
+
+}
