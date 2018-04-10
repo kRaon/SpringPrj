bece65a84890aca16da396ae9361447b320fcd87
tatagi <tatagi@naver.com>
2018-04-10 17:03:46 +0900

    push

--- app/src/main/java/com/my/web/controller/LoginController_backup.java
+++ app/src/main/java/com/my/web/controller/LoginController_backup.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.google.gson.JsonObject;
